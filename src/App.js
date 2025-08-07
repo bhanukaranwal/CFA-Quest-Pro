80e50a58762687cc758ad82c5e53fc239cc4dc7f
Bhanu Karnwal <74866394+bhanukaranwal@users.noreply.github.com>
2025-08-08 02:53:50 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import ExamInterface from './components/ExamInterface';
+import Dashboard from './components/Dashboard';
+import Achievements from './components/Achievements';
+
+function App() {
+  return (
+      <Router>
+        <div className="min-h-screen bg-secondary font-sans">
+          <Navbar />
+          <main>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/exam" element={<ExamInterface />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/achievements" element={<Achievements />} />
+            </Routes>
+          </main>
+        </div>
+      </Router>
+  );
+}
+
+export default App;
